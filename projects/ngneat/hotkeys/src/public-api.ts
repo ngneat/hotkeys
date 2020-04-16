befb2d87636fa720434654218efc21f0d63d6c89
flatstadt <cvilachap@gmail.com>
2020-04-16 07:48:56 +0200

    removed unused global config

--- projects/ngneat/hotkeys/src/public-api.ts
+++ projects/ngneat/hotkeys/src/public-api.ts
@@ -6,5 +6,4 @@ export * from './lib/hotkeys.service';
 export * from './lib/hotkeys.directive';
 export * from './lib/hotkeys-help/hotkeys-help.component';
 export * from './lib/hotkeys-shortcut.pipe';
-export * from './lib/hotkeys-config.service';
 export * from './lib/hotkeys.module';
