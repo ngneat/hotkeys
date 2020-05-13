6d3922297fc071ce72baa681e3d5789c4e2b4e10
Netanel Basal <netanel7799@gmail.com>
2020-05-13 14:09:57 +0300

    fix: 🐛 remove material

--- projects/ngneat/hotkeys/src/lib/hotkeys.module.ts
+++ projects/ngneat/hotkeys/src/lib/hotkeys.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { HotkeysShortcutPipe } from './hotkeys-shortcut.pipe';
 import { HotkeysHelpComponent } from './hotkeys-help/hotkeys-help.component';
@@ -9,7 +8,7 @@ import { HotkeysDirective } from './hotkeys.directive';
 @NgModule({
   declarations: [HotkeysDirective, HotkeysHelpComponent, HotkeysShortcutPipe],
   entryComponents: [HotkeysHelpComponent],
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule],
   exports: [HotkeysDirective, HotkeysHelpComponent, HotkeysShortcutPipe]
 })
 export class HotkeysModule {}
