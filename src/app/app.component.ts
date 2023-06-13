c4c69f1b649dc7a666325a5c030f98c3aaa01b90
Richard Russell <2265225+rars@users.noreply.github.com>
2023-06-13 21:21:12 +0100

    fix!: change property `dimiss` to `dismiss` in help component output
    BREAKING CHANGE: `dimiss` property has been renamed to `dismiss` in help component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements AfterViewInit {
     const helpFcn: () => void = () => {
       const ref = this.dialog.open(HotkeysHelpComponent, { width: '500px' });
       ref.componentInstance.title = 'Custom Shortcuts Title';
-      ref.componentInstance.dimiss.subscribe(() => ref.close());
+      ref.componentInstance.dismiss.subscribe(() => ref.close());
     };
 
     this.hotkeys.registerHelpModal(helpFcn);
